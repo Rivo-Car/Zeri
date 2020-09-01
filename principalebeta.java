14f4385bc4d1e17043150365b1b334bafb3f59a3
Rivo-Car <70138793+Rivo-Car@users.noreply.github.com>
2020-09-01 23:45:57 +0300

    Update principalebeta.java

--- principalebeta.java
+++ principalebeta.java
@@ -1,46 +1,8 @@
+import net.objecthunter.exp4j.*;
+import java.util.*;
 
-import java.util.Scanner;
-import java.util.InputMismatchException;
-import exp4j.*;
 import Metodi.*;
-
-
-
-//   javac -classpath .:lib/jep-2.4.1.jar:progettoInfo.jar principale.java
-//   java -classpath .:lib/jep-2.4.1.jar:progettoInfo.jar principale
-
-
-//   javadoc *.java -d documentazione/ -private -classpath .:lib/jep-2.4.1.jar -link https://docs.oracle.com/javase/8/docs/api/
-//   javadoc *.java -d documentazione/ -classpath .:lib/jep-2.4.1.jar -link https://docs.oracle.com/javase/8/docs/api/
-//   javadoc progettoInfo/*.java -d documentazione/ -classpath .:lib/jep-2.4.1.jar -link https://docs.oracle.com/javase/8/docs/api/
-
-
-
-//    jar cf progettoInfo.jar progettoInfo/*.class
-
-
-
-
-
-/**
-* classe contenente il main del programma.
-* <br>
-* Il programma e' stato sviluppato per essere interattivo con l'utente, con l'obiettivo di risolvere
-* delle equazioni del tipo f(x)=..., trovando una x' tale che f(x')~=0.
-* <br>
-* Questo programma fa uso della libreria <a href="http://www.singularsys.com/jep/download-trial.php">jep-2.4.1</a>, in particolare sfrutta gli oggetti della classe <a href="http://www.singularsys.com/jep/doc/javadoc/index.html">JEP</a> e della classe DJep.
-* <br>
-* I metodi disponibili sono: bisezione, Newton, corde, secanti, Laguerre, punto fisso.
-* <br>
-* <b> NB:</b> in Laguerre bisogna inserire un polinomio
-* <br>
-* <b> NB:</b> in punto fisso bisogna inserire la g(x) tale che f(x')~=0 se g(x')~=x'
-*
-* @author Alessandro Reami
-* @version 3.0
-*/
-public class principalebeta{
-
+public class Zeri {
 	private static Scanner tastiera = new Scanner(System.in);
 	private static Scanner tastiera2= new Scanner(System.in);
 	private static Scanner tastiera3= new Scanner(System.in);
@@ -53,8 +15,8 @@ public class principalebeta{
 	public static double a,b,toll_err;
 	//le chiamo così perchè  a seconda del caso saranno estremo destro e sinistro, o primo e secondo punto di applicazione dei metodi
 
-	public static double zero;
-	public static double resid;
+	public static double zero = 0;
+	public static boolean caso_errore = true;
 	
 	/**
 	* Metodo main
@@ -73,7 +35,6 @@ public class principalebeta{
 		
 		metodo=tastiera.nextLine();
 		zero= Double.NaN;
-		resid= Double.NaN;
 
 		if(metodo.equals("bisezione")||metodo.equals("Bisezione")||metodo.equals("BISEZIONE")){
 			 do {
@@ -157,7 +118,7 @@ public class principalebeta{
 
 
 			else if(metodo.equals("Chiudi")||metodo.equals("chiudi")||metodo.equals("CHIUDI")){
-				System.out.println("\nGrazie mille!\n Buona giornata\n\n");
+				System.out.println("\nGrazie mille!\nBuona giornata\n\n");
 				acceso = false;
 				}
 			else {
@@ -173,6 +134,7 @@ public class principalebeta{
 	}
 	
 	public static void input1(){
+		tastiera.reset();
 	try{
 			System.out.println("Inserire la funzione di cui calcolare gli zeri: ");
 			fg=tastiera.nextLine();
@@ -184,49 +146,86 @@ public class principalebeta{
 	}
 	
 	public static void inputf(){
+		tastiera.reset();
 	try{	
 		System.out.println("Inserire la funzione di cui calcolare gli zeri: ");
 		fg=tastiera.nextLine();
 		Fun = new Function(fg);
 	} catch(InputMismatchException e ){ System.err.println("valori di input non validi ");}
 		
-	}
+   }
 	
 	public static void input2(){
-		try{
+	
+		do 
+			try {
+				
 			System.out.println("inserire l'estremo sinistro \n in caso si usi il metodo delle secanti, inserire il primo punto di partenza ");
 			a=tastiera2.nextDouble();
+			
 			System.out.println("inserire l'estremo destro \n in caso si usi il metodo delle secanti, inserire il secondo punto di partenza ");
 			b=tastiera2.nextDouble();
-		}
-			catch(InputMismatchException e ){ System.err.println("valori di input non validi ");}
-		}
+			
+			caso_errore = false;
+			
+		    }
+			catch(InputMismatchException e ){ System.err.println("valori di input non validi "); tastiera2.next();}
+		
+        while (caso_errore != false);
+		
+		caso_errore = true;
+	}
 	
 	public static void input3(){
-		try {
+
+		do 
+			try {
 			System.out.println("inserire la tolleranza desiderata ");
 			toll_err=tastiera2.nextDouble();
+			
 			System.out.println("inserire il numero massimo di iterazioni ");
 			numero_max_iter=tastiera2.nextInt();
+			
+			caso_errore = false;
 			}
-			catch(InputMismatchException e ){ System.err.println("valori di input non validi ");}
+			catch(InputMismatchException e ){ System.err.println("valori di input non validi, riprovare: "); tastiera2.next();}
+		
+		while (caso_errore != false);
+		
+		caso_errore = true;
 	}
 	
 	public static void input4(){
-		try{
+		do
+	    	try{
+	    		
 			System.out.println("Inserire il nodo di partenza ");
 			a=tastiera2.nextDouble();
+			
+			caso_errore = false;
+			
+		  } catch(InputMismatchException e ){ System.err.println("valori di input non validi "); tastiera2.next(); }
+		
+		while (caso_errore != false);
 
-		} catch(InputMismatchException e ){ System.err.println("valori di input non validi ");}
-
+		caso_errore = true;
 	}
 	
 	public static void input5(){
-		try{
+		do
+	    	try{
+	    		
 			System.out.println("inserire la molteplicità degli zeri da trovare ");
 			mult=tastiera2.nextInt();
-		} catch(InputMismatchException e ){System.err.println("valore in input non valido ");}
-		}
+			
+			caso_errore = false;
+			
+		  } catch(InputMismatchException e ){System.err.println("valore in input non valido "); tastiera2.next(); }
+		
+		while (caso_errore != false);
+
+		caso_errore = true;
+	}
 	
 	public static boolean ripassafunzione() {
 		
@@ -243,5 +242,4 @@ public class principalebeta{
 			    return false;
 	    }
 	}
-   
-	}
+  }
